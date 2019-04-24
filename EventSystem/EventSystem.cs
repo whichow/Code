c02b8e81b81b1a05197815bd9713919fa9ef9f4a
whichow <whichow@hotmail.com>
2019-04-24 16:36:31 +0800

    EventSystem

---
+++ EventSystem/EventSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Event
+{
+    public class EventSystem
+    {
+        public EventSystem()
+        {
+
+        }
+
+        /// <summary>
+        /// 注册事件
+        /// </summary>
+        /// <param name="listener">事件监听者</param>
+        public void RegisterEvent(IEventListener listener)
+        {
+
+        }
+
+        /// <summary>
+        /// 发送事件，并移除已销毁的监听者
+        /// </summary>
+        /// <param name="eventType">事件类型</param>
+        /// <param name="eventArgs">事件数据</param>
+        public void SendEvent(string eventType, params object[] eventArgs)
+        {
+
+        }
+    }
+}
---
+++ EventSystem/IEventListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Event
+{
+    public interface IEventListener
+    {
+        /// <summary>
+        /// 监听者的优先级
+        /// </summary>
+        int EventPriority { get; }
+        /// <summary>
+        /// 是否阻止事件传递
+        /// </summary>
+        bool BlockEvent { get; }
+        /// <summary>
+        /// 要监听的事件
+        /// </summary>
+        string[] ListenEvents { get; }
+        /// <summary>
+        /// 处理事件
+        /// </summary>
+        /// <param name="eventType">事件类型</param>
+        /// <param name="eventArgs">事件参数</param>
+        void HandEvent(string eventType, params object[] eventArgs);
+    }
+}
